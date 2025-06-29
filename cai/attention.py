9c7b9a289a9b8f6cfa3f5dfbc0641af9939cda07
Hussain-SZ <hussain.sulaiman3@yahoo.com>
2025-06-29 12:42:45 +0500

    Update attention and inception

--- cai/attention.py
+++ cai/attention.py
@@ -1,77 +1,91 @@
 # In file: k/cai/attention.py
 
 import tensorflow as tf
-from tensorflow.keras import layers, initializers
+from tensorflow.keras import layers, models
 
-# --- HELPER FUNCTION MOVED TO THE TOP-LEVEL ---
-# By being a global function in the module, Keras can find it during model loading.
-def spatial_output_shape(input_shape):
-    """
-    Calculates the output shape for spatial attention, which is (batch, height, width, 1).
-    """
-    return input_shape[:-1] + (1,)
-# --------------------------------------------
+@tf.keras.utils.register_keras_serializable(package='Cai')
+class ChannelAttention(layers.Layer):
+    def __init__(self, ratio=8, **kwargs):
+        super(ChannelAttention, self).__init__(**kwargs)
+        self.ratio = ratio
 
-def channel_attention(input_feature, ratio=8):
-    """
-    Applies Channel Attention to the input feature map.
-    """
-    channel = input_feature.shape[-1]
-    
-    shared_layer_one = layers.Dense(channel // ratio,
-                                    activation='relu',
+    def build(self, input_shape):
+        channel = input_shape[-1]
+        self.shared_layer_one = layers.Dense(channel // self.ratio,
+                                             activation='relu',
+                                             kernel_initializer='he_normal',
+                                             use_bias=True,
+                                             bias_initializer='zeros')
+        self.shared_layer_two = layers.Dense(channel,
+                                             kernel_initializer='he_normal',
+                                             use_bias=True,
+                                             bias_initializer='zeros')
+        super(ChannelAttention, self).build(input_shape)
+
+    def call(self, inputs):
+        # AvgPool
+        avg_pool = layers.GlobalAveragePooling2D()(inputs)
+        avg_pool = layers.Reshape((1, 1, avg_pool.shape[1]))(avg_pool)
+        avg_pool = self.shared_layer_one(avg_pool)
+        avg_pool = self.shared_layer_two(avg_pool)
+        # MaxPool
+        max_pool = layers.GlobalMaxPooling2D()(inputs)
+        max_pool = layers.Reshape((1, 1, max_pool.shape[1]))(max_pool)
+        max_pool = self.shared_layer_one(max_pool)
+        max_pool = self.shared_layer_two(max_pool)
+        # Combine
+        cbam_feature = layers.Add()([avg_pool, max_pool])
+        cbam_feature = layers.Activation('sigmoid')(cbam_feature)
+        return inputs * cbam_feature
+
+    def get_config(self):
+        config = super(ChannelAttention, self).get_config()
+        config.update({'ratio': self.ratio})
+        return config
+
+@tf.keras.utils.register_keras_serializable(package='Cai')
+class SpatialAttention(layers.Layer):
+    def __init__(self, kernel_size=7, **kwargs):
+        super(SpatialAttention, self).__init__(**kwargs)
+        self.kernel_size = kernel_size
+        self.conv2d = layers.Conv2D(filters=1,
+                                    kernel_size=self.kernel_size,
+                                    strides=1,
+                                    padding='same',
+                                    activation='sigmoid',
                                     kernel_initializer='he_normal',
-                                    use_bias=True,
-                                    bias_initializer='zeros')
-    shared_layer_two = layers.Dense(channel,
-                                    kernel_initializer='he_normal',
-                                    use_bias=True,
-                                    bias_initializer='zeros')
-    
-    avg_pool = layers.GlobalAveragePooling2D()(input_feature)
-    avg_pool = layers.Reshape((1, 1, channel))(avg_pool)
-    avg_pool = shared_layer_one(avg_pool)
-    avg_pool = shared_layer_two(avg_pool)
-    
-    max_pool = layers.GlobalMaxPooling2D()(input_feature)
-    max_pool = layers.Reshape((1, 1, channel))(max_pool)
-    max_pool = shared_layer_one(max_pool)
-    max_pool = shared_layer_two(max_pool)
-    
-    cbam_feature = layers.Add()([avg_pool, max_pool])
-    cbam_feature = layers.Activation('sigmoid')(cbam_feature)
-    
-    return layers.multiply([input_feature, cbam_feature])
+                                    use_bias=False)
 
-def spatial_attention(input_feature, kernel_size=7):
-    """
-    Applies Spatial Attention to the input feature map.
-    """
-    cbam_feature = input_feature
+    def call(self, inputs):
+        avg_pool = tf.reduce_mean(inputs, axis=3, keepdims=True)
+        max_pool = tf.reduce_max(inputs, axis=3, keepdims=True)
+        concat = layers.Concatenate(axis=3)([avg_pool, max_pool])
+        spatial_feature = self.conv2d(concat)
+        return inputs * spatial_feature
 
-    # The Lambda layers now correctly reference the top-level helper function
-    avg_pool = layers.Lambda(lambda x: tf.reduce_mean(x, axis=3, keepdims=True),
-                             output_shape=spatial_output_shape)(cbam_feature)
-                             
-    max_pool = layers.Lambda(lambda x: tf.reduce_max(x, axis=3, keepdims=True),
-                             output_shape=spatial_output_shape)(cbam_feature)
+    def get_config(self):
+        config = super(SpatialAttention, self).get_config()
+        config.update({'kernel_size': self.kernel_size})
+        return config
 
-    concat = layers.Concatenate(axis=3)([avg_pool, max_pool])
-    
-    cbam_feature = layers.Conv2D(filters=1,
-                                 kernel_size=kernel_size,
-                                 strides=1,
-                                 padding='same',
-                                 activation='sigmoid',
-                                 kernel_initializer='he_normal',
-                                 use_bias=False)(concat)
-    
-    return layers.multiply([input_feature, cbam_feature])
+@tf.keras.utils.register_keras_serializable(package='Cai')
+class CBAMBlock(layers.Layer):
+    def __init__(self, ratio=8, kernel_size=7, **kwargs):
+        super(CBAMBlock, self).__init__(**kwargs)
+        self.ratio = ratio
+        self.kernel_size = kernel_size
+        self.channel_attention = ChannelAttention(ratio=self.ratio)
+        self.spatial_attention = SpatialAttention(kernel_size=self.kernel_size)
 
-def cbam_block(cbam_feature, ratio=8):
-    """
-    The complete Convolutional Block Attention Module (CBAM).
-    """
-    cbam_feature = channel_attention(cbam_feature, ratio)
-    cbam_feature = spatial_attention(cbam_feature)
-    return cbam_feature
+    def call(self, inputs):
+        x = self.channel_attention(inputs)
+        x = self.spatial_attention(x)
+        return x
+
+    def get_config(self):
+        config = super(CBAMBlock, self).get_config()
+        config.update({
+            'ratio': self.ratio,
+            'kernel_size': self.kernel_size
+        })
+        return config
