800e305d74a50aee661a9e5beb712ffd982a6b4f
Hussain-SZ <hussain.sulaiman3@yahoo.com>
2025-07-06 21:26:58 +0500

    Edit the call() func

--- cai/color_layers.py
+++ cai/color_layers.py
@@ -15,15 +15,10 @@ class RgbToLab(layers.Layer):
         super(RgbToLab, self).__init__(**kwargs)
 
     def call(self, inputs):
-        # The input RGB values are expected to be in the range [0, 1].
-        # We need to convert them to integers in the range [0, 255] for the conversion function.
-        rgb_int = tf.image.convert_image_dtype(inputs, dtype=tf.uint8)
-        
-        # Convert from RGB to LAB.
-        lab_image = tfio.experimental.color.rgb_to_lab(rgb_int)
-        
-        # The output of the conversion is float, so we cast it back to the input's dtype.
-        return tf.cast(lab_image, inputs.dtype)
+        # The input tensor is already float32, which is what rgb_to_lab expects.
+        # The incorrect conversion to uint8 has been removed.
+        lab_image = tfio.experimental.color.rgb_to_lab(inputs)
+        return lab_image
 
     def get_config(self):
         # Required for model saving and loading.
